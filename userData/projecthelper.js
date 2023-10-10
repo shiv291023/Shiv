f1eaedb69fce7de950951b92b867e0ba13b0464a
shiv291023 <saror139@my.centennialcollege.ca>
2023-10-10 09:57:33 -0400

    git commit

---
+++ userData/projecthelper.js
@@ -0,0 +1,193 @@
+module.exports = [
+  // {
+  //   id: 1,
+  //   name: "Portfolio Website",
+  //   desc: "My Portfolio Website. A tempting UX/UI inspired by the top Portfolios. You are already there !",
+  //   image: "/images/projects/portfolio.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 2,
+  //   name: "Hike-clone",
+  //   desc: "A Project based on React-native tech stack express of React with . One of the top full stack App project ever Made !!",
+  //   image: "/images/projects/yelpcamp.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 4,
+  //   name: "Food Ordering App",
+  //   desc: "An app based on React-native, Routes as well as Star button where we can save our favour dish. It is still under development but do check it out.",
+  //   image: "/images/projects/food-app.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 5,
+  //   name: "Weather web",
+  //   desc: "Weather web is a basic website where we enter a city,state name from anywhere we get a current weather status",
+  //   image:
+  //     "https://user-images.githubusercontent.com/55958223/130330130-34bf4ffc-cd60-4fc8-87e0-5ac3f455086b.jpeg",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 6,
+  //   name: "Online Notes Generator(todo-list)",
+  //   desc: "Basic-web of showcase a list which we made for us  !!",
+  //   image: "/images/projects/onlineNotesGenerator.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 7,
+  //   name: "Color-changer",
+  //   desc: "Basic web on click change of background - color",
+  //   image: "/images/projects/onlinebanking.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 8,
+  //   name: "Coffee web",
+  //   desc: "Here is a Coffee page where is can buy and sale a coffee beans  ",
+  //   image: "/images/projects/paymentgateway.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 9,
+  //   name: "Rock Paper Scissors",
+  //   desc: " Online Normal Game ",
+  //   image: "/images/projects/pepper.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 10,
+  //   name: "Expense handler (React)",
+  //   desc: "Expense handler based on React. It has expense bars as well year, month filter. A lot of useState !!",
+  //   image: "/images/projects/expensehandler.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 11,
+  //   name: "Employee Fetch & Store (Mongoose)",
+  //   desc: "A prefect guide to Restful routes of how firebase storage works with Axios for CRUD requests",
+  //   image: "/images/projects/movieStorage.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 12,
+  //   name: "Sneaker Shop",
+  //   desc: "A Sneaker app where we can purchase a shoes through App ",
+  //   image: "/images/projects/mobileshop.png",
+  //   links: {
+  //     view: "https://mobile-shop-india.herokuapp.com/",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 13,
+  //   name: "CRUD",
+  //   desc: "Node.js app it is a basic web where we can create update nd delete a functionality",
+  //   image: "/images/projects/monsterrolodex.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 14,
+  //   name: "Express + MongoDB + Mongoose",
+  //   desc: "Employee Salary Account where we manage a Employee Token id then we distribute Salary!!",
+  //   image: "/images/projects/expressmongoose.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 15,
+  //   name: "Rabbit NEWS",
+  //   desc: "This is React-website, Rabbit NEWS website where we see and get Update ourself from the website ",
+  //   image: "/images/projects/stopwatchapp.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/Anunay-Anand/stop-watch",
+  //   },
+  // },
+  // {
+  //   id: 16,
+  //   name: "Rabbit-Movies",
+  //   desc: "This is a React Movie basic website where we can easily watch a new movie in this website we fetch API too ",
+  //   image: "/images/projects/languageguesser.png",
+  //   links: {
+  //     view: "#",
+  //     code: "https://github.com/nikhilseth30",
+  //   },
+  // },
+  // {
+  //   id: 17,
+  //   name: "Score Keeper",
+  //   desc: "A Simple JavaScript, Css and HTML website with score keeping and Reset functionality.",
+  //   image: "/images/projects/scorekeeper.png",
+  //   links: {
+  //     view: "https://anunay-anand.github.io/ScoreKeeper/",
+  //     code: "https://github.com/Anunay-Anand/ScoreKeeper",
+  //   },
+  // },
+  // {
+  //   id: 18,
+  //   name: "Lovely Project",
+  //   desc: "A beautiful project with video background fully responsive with Bootstrap and Media queries.",
+  //   image: "/images/projects/lovelyproject.png",
+  //   links: {
+  //     view: "https://anunay-anand.github.io/myLovelyProject/",
+  //     code: "https://github.com/Anunay-Anand/myLovelyProject",
+  //   },
+  // },
+  // {
+  //   id: 19,
+  //   name: "Fruit game",
+  //   desc: "It is a very low budget rip off of fruit ninja with sounds and score keeping.",
+  //   image: "/images/projects/fruitninja.png",
+  //   links: {
+  //     view: "https://anunay-anand.github.io/fruits-game/",
+  //     code: "https://github.com/Anunay-Anand/fruits-game",
+  //   },
+  // },
+  // {
+  //   id: 20,
+  //   name: "Math game",
+  //   desc: "It is a basic maths multiplication game using javaScript. It has a timer and scorekeeper",
+  //   image: "/images/projects/math.png",
+  //   links: {
+  //     view: "https://anunay-anand.github.io/MathsGame/",
+  //     code: "https://github.com/Anunay-Anand/MathsGame",
+  //   },
+  // },
+];
